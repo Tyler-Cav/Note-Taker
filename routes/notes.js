0be0fc13e4eb80cc9efa916f681e21f0a23d3b32
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-02 22:27:13 -0500

    added readme info

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 router.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then(data=> res.json(JSON.parse(data))) 
+    readFromFile('./db/db.json').then(data => res.json(JSON.parse(data))) 
 })
 router.post('/notes', (req, res) => {
     const {title, text} = req.body
